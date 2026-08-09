a8e83aa0b9675f28019c6d41ef3c98be6115d6d9
aishpaym <aishwaryasasankan@gmail.com>
2026-08-09 11:34:14 +0530

    Add Playwright automation tests

--- tests/ClientTest.spec.ts
+++ tests/ClientTest.spec.ts
@@ -13,7 +13,7 @@ import { test, expect } from '@playwright/test';
 
 //When we need to run only one testcase out of all of the testcase
 
-test.only('Page Playwright test', async({page})=>{
+test('Page Playwright test', async({page})=>{
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     //get - title --> Assertion
     console.log(await page.title());
@@ -33,13 +33,16 @@ test.only('Page Playwright test', async({page})=>{
     //await expect(await page.locator("[style*='block']")).toContainText('Learning');
     //----storing it in variable-----//
     //store the locator into one variable
+    const email="aishwaryasasankan@gmail.com";
     const userEmail=page.locator("#userEmail");
     const password=page.locator("#userPassword");
     const cardTitle=page.locator(".card-body b");
     const signin =page.locator("#login");
+    const products=page.locator(".card-body");
+    const productName='iphone 13 pro';
 
-    await userEmail.fill("anshika@gmail.com");
-    await password.fill("Iamking@000");
+    await userEmail.fill(email);
+    await password.fill("S3F5pMaK62!.jWr");
     
     await signin.click();
     //Here the locator has multiple elements so to do out of 4 element we will take it in array and give it which index we need that is nth[0] or we can use first() or last()
@@ -51,11 +54,113 @@ test.only('Page Playwright test', async({page})=>{
     //await page.waitForLoadState("networkidle");
     //sometimes the above step seems to be flaky so there is an alternative solution for this and we can use a different method
     //Alternative soluntion is waitfor() the locator to load --> But this command does not understand till when it has to wait so we uses first.waitfor() or last.wairfor()
-    await cardTitle.last().waitFor();
+    await cardTitle.last().waitFor();//Need to wait for cards to load
     const allTitles=await cardTitle.allTextContents();
     console.log(allTitles);
+    const count= await products.count();
+    for(let i=0;i<count;++i){
+        //To seach for a particular element from the loop the below lines will search
+        //This will be restricted scope of search
+        if (await products.nth(i).locator("b").textContent()===productName){
+            //add to cart 
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+
+        }
+    }
+
+//Now we need to click on the cart and verify we can use attribute = value
+const cartbutton=page.locator("[routerlink*='cart']");
+await cartbutton.waitFor();
+await cartbutton.click();
+
+//await page.pause();
+//before checking the text in card need to wait for page
+await page.locator(".cart li").waitFor();
+// has text will give boolen values
+const bool= await page.locator("h3:has-text('iphone 13 pro')").isVisible();
+expect(bool).toBeTruthy();
+const checkout=await page.locator("text= Checkout").click();
+//await page.pause();
+
+//Fill Card Details
+const cardnumber= await page.locator("input.input.txt.text-validated").first();
+const expirydate= await page.locator("select.input.ddl").first();
+const expiryyear= await page.locator("select.input.ddl").last();
+const cvvInput = page.locator('div.field.small').filter({ hasText: 'CVV Code' }).locator('input[type="text"]');
+const nameoncard= await page.locator("div.field").filter({ hasText: 'Name on Card '}).locator('input[type="text"]');
+const applycoupan= await page.locator("div.field.small").filter({ hasText : 'Apply Coupon '}).locator('input[name="coupon"]');
+//const clickapply= await page.locator("button[type='submit']");
+//The given below is a suggestive dropdown and we will check how to handle it for that we have this locator called pressSequentially
+const country= await page.locator("[placeholder*='Country']");
+
+//for selecting from the suggestive dropdown 
+const dropdown= await page.locator(".ta-results");
+const proceedbtn = await page.locator(".action__submit");
+
+await cardnumber.fill("8754 1236 9785 5485");
+await expirydate.selectOption({label : "03"});
+await expiryyear.selectOption({label : "28"});
+await cvvInput.fill("123");
+await nameoncard.fill("Aishwarya Karan");
+await applycoupan.fill("Apply");
+await country.pressSequentially("ind",{ delay: 150 });
+await dropdown.waitFor();
+ const dropdowncount= await dropdown.locator("button").count();
+ for(let i=0;i<dropdowncount;++i){
+    const text = await dropdown.locator("button").nth(i).textContent();
+
+    //we can use text.trim() in case we dont need the spaces In TypeScript, you trim a string by calling the built-in string.trim() method, which removes all leading and trailing whitespace (including spaces, tabs, and newlines) and returns a new string without altering the original one.
+    if(text === " India"){
+        //need to click operation on that option
+        await dropdown.locator("button").nth(i).click();
+        break;
+    }
+ }
+await expect(page.locator("div.user__name input[type='text']").first()).toHaveValue(email);
+await proceedbtn.click();
+//Move to Next Page
+await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+const orderID= await page.locator(".em-spacer-1 .ng-star-inserted").textContent(); 
+console.log(orderID);
+//await page.pause();
+//Now moving to the orderID page
+
+const orders=await page.locator("[routerlink *='myorders']").first();
+await orders.click();
+//await page.pause();
+
+//need to check in order details screen
+//TypeScript understands that orderID is definitely a string, so this works:
+//most important topic
+if (!orderID) {
+  throw new Error('Order ID was not found');
+}
+await page.locator("tbody").waitFor();
+
+const orderrow = await page.locator("tbody tr");
+ console.log(await orderrow.count());
+
+for(let i =0;i< await orderrow.count(); i++){
+    const ordervalue= (await orderrow.nth(i).locator("th").textContent())?.trim() ?? "";
+    if(orderID.trim().includes(ordervalue)){
+        await orderrow.nth(i).locator("button").first().click();
+        break;
+
+    }
+}
+//----------------Verify details in Order Summary Screen-----------------------
+
+//get the order ID from order Summary Screen
+const ordersummvalue=await page.locator("div.col-text").textContent();
+if (!ordersummvalue) {
+  throw new Error('Order ID was not found');
+}
+if (ordersummvalue.includes(orderID)){
+    console.log("It is the correct order ID");
+}
 
 
-
+await page.pause();
 
 });
--- tests/UIBasicstest.spec.ts
+++ tests/UIBasicstest.spec.ts
@@ -14,7 +14,7 @@ test('First Playwright test', async ({browser})=>{
 
 //When we need to run only one testcase out of all of the testcase
 
-test.only('Page Playwright test', async({page})=>{
+test('Page Playwright test', async({page})=>{
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     //get - title --> Assertion
     console.log(await page.title());
@@ -38,13 +38,14 @@ test.only('Page Playwright test', async({page})=>{
     const password=page.locator("#password");
     const cardTitle=page.locator(".card-body a");
     const signin =page.locator("[name='signin']");
-
+    
     await username.fill("rahulshettyacademy");
     await password.fill("Learning@830$3mK2");
     
     await signin.click();
     //Here the locator has multiple elements so to do out of 4 element we will take it in array and give it which index we need that is nth[0] or we can use first() or last()
     //console.log(await page.locator(".card-body a").first().textContent());
+    //In place of textContent we can also use inputValue()
     console.log(await cardTitle.nth(1).textContent());
     //To grab all the titles
     //if we comment out that nth line and then try to execute there would be error with the allTextContents method as there is no action defined for it
@@ -54,4 +55,70 @@ test.only('Page Playwright test', async({page})=>{
 
 
 
+});
+
+//test for UI Controls
+test('UI Controls', async({page})=>{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    //Now to practise dropdowns
+    //One is static dropdown or select dropdown
+    const username=page.locator("#username");
+    const password=page.locator("#password");
+    await username.fill("rahulshettyacademy");
+    await password.fill("Learning@830$3mK2");
+    //if the class name is not unique concatenate it with tagname that is select.form-control Here select is the tagname of the element and .form-control is classname
+    const dropdown=page.locator("select.form-control");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click()
+    await dropdown.selectOption("consult");
+    //assertion
+    //To check that the radiobutton is checked or not
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+    //to print True/False after checking if the radio button is checked or not but it is not of much of use in real time this for the developer to use
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    await page.locator("#terms").click();
+    //We need to prepare this step as an assertions
+    await expect(page.locator("#terms")).toBeChecked();
+    //Just check if the radio button is unchecked also
+    await page.locator("#terms").uncheck();
+    //Then verify what is the result and here we are checking it to be False only
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();
+
+
+
+
+    //To see the execution completely we can use the below function
+    await page.pause();
+
+});
+
+test("Child window tab", async({browser})=>{
+    //This 2 const are important to be declared here
+     const contxt = await browser.newContext(); //on this only browser instance is open so to create a page into it we need the next line
+     const page = await contxt.newPage(); 
+     //----------------------
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink=page.locator("[href*='documents-request']");
+     //To switch to new tab we need to create a new context
+//waitforevent method waits for page to open in the background and if it opens it returns that to the variable 
+   const [newpage]= await Promise.all(
+    [contxt.waitForEvent('page'),//listen for any new page to open ->States -> pending,rejected,fullfilled
+        //This method needs to be open before the click option
+    documentLink.click()
+]);//Promise.all() -> This takes some array of promises this is when we need to execute 2 function parallely execute  
+    //This click open a separate window so we will have no direct access to it so before click we need to playwright that we need to wait for a new page to generate
+    await newpage.waitForLoadState('networkidle'); 
+    const text= await newpage.locator(".red").textContent();
+     //Sometimes textContent() returns null.
+     if(text){
+     const arraytoval=await text.split("@");
+     const email= await arraytoval[1].split(" ")[0];
+     console.log(email);
+    //if we want the value from page 2 in page 1 
+     await page.locator("#username").fill(email);
+     await page.pause();
+
+
+    
+    }
 });
